2c7a55fc8edc4e3dd312a9fc9cf6fe659203beee
Matt Mower <mowerm@gmail.com>
2015-05-27 17:52:07 -0500

    Initial git migration, use version control
    Take advantage of version control instead of dumping new versions into
    separate folders. Initial contribution is equivalent to 3.7.24.

--- 2.01/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" class="searchform" action="<?php bloginfo('url'); ?>/">
-<input type="text" style="width: 99%" value="<?php the_search_query(); ?>" name="s" />
-<!--<input name="submit" value="Search" type="submit" style="vertical-align: bottom; margin: 0;" />-->
-</form>
